1f70ce46225800378ccb3a1d2baa7e03cf6ab3be
RuiterM <302078305@student.rocmondriaan.nl>
2023-01-25 22:45:08 +0100

    Update admin.php

--- public/admin.php
+++ public/admin.php
@@ -2,8 +2,9 @@
 global $params;
 
 //check if user has role admin
-if ($_SESSION['user']->role!="admin") {
-    echo "faya";
+if (!isAdmin()) {
+    logout();
+    header ("location:/home");
 } else {
     /* $params[2] is de action
        $params[3] is een getal die de delete action nodig heeft
@@ -11,16 +12,11 @@ if ($_SESSION['user']->role!="admin") {
     switch ($params[2]) {
 
         case 'home':
-            include_once '../Templates/admin/adminhome.php';
+            include_once "../Templates/admin/adminhome.php";
             break;
-
-        case 'products':
-            break;
-
-        case 'add':
-            break;
-
-        case 'delete':
+        case 'logout':
+            logout();
+            header("Location: /home");
             break;
 
         default:

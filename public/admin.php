e90da6b33b4546cac0f82bfa9a699a9cfe1a555e
RuiterM <302078305@student.rocmondriaan.nl>
2023-01-25 22:43:26 +0100

    Update admin.php

--- public/admin.php
+++ public/admin.php
@@ -2,53 +2,28 @@
 global $params;
 
 //check if user has role admin
-<<<<<<< Updated upstream
-if (!isAdmin()) {
-    logout();
-    header ("location:/home");
+if ($_SESSION['user']->role!="admin") {
+    echo "faya";
 } else {
-/* $params[2] is de action
-   $params[3] is een getal die de delete action nodig heeft
-*/
+    /* $params[2] is de action
+       $params[3] is een getal die de delete action nodig heeft
+    */
     switch ($params[2]) {
 
         case 'home':
-            include_once '../Templates/admin/home.php';
+            include_once '../Templates/admin/adminhome.php';
             break;
-=======
-if ($_SESSION['user']->role!=="admin") {
-    echo "faya";
-    if (!isAdmin()) {
-        logout();
-        header("location:/home");
-    } else {
-        /* $params[2] is de action
-           $params[3] is een getal die de delete action nodig heeft
-        */
 
-        switch ($params[2]) {
->>>>>>> Stashed changes
+        case 'products':
+            break;
 
-            case 'home':
-                include_once '../Templates/admin/adminhome.php';
-                break;
+        case 'add':
+            break;
 
-            case 'products':
-                include_once "../Templates/admin/adminhome.php";
-                break;
+        case 'delete':
+            break;
 
-            case 'add':
-                break;
-
-            case 'delete':
-
-
-            case 'logout':
-                logout();
-                header("Location: /home");
-                break;
-
-            default:
-        }
+        default:
+            break;
     }
 }

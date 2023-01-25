ea80d06c2f74b24eeb0083bd296422899acfd374
RuiterM <302078305@student.rocmondriaan.nl>
2023-01-25 22:38:53 +0100

    contact

--- Templates/login.php
+++ Templates/login.php
@@ -1,54 +1,7 @@
 <?php
-<<<<<<< Updated upstream
-$melding = "";
-$db = new PDO('mysql:host=localhost;dbname=auto4u',
-    "root" . "");
-$query = $db->prepare("select * FROM cards");
-$query->execute();
-$autos = $query->fetchAll(PDO::FETCH_ASSOC);
-
-if (isset($_POST['Login'])) {
-    if (!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['firstName']) && !empty($_POST['lastName'])) {
-
-        $Email = filter_input(INPUT_POST,'email', FILTER_VALIDATE_EMAIL);
-        $Password = filter_input(INPUT_POST,'password', FILTER_SANITIZE_STRING);
-        $FirstName = filter_input(INPUT_POST,'firstName');
-        $LastName = filter_input(INPUT_POST,'lastName');
-        $hashed_password = password_hash($Password, PASSWORD_DEFAULT);
-
-        if ($Email === false) {
-            $melding = "vul een email in!";
-        }else {
-
-            $Email = $_POST['email'];
-            $hashed_password = $_POST['password'];
-            $FirstName = $_POST['firstName'];
-            $LastName = $_POST['lastName'];
-
-            $query = $db->prepare("INSERT INTO accounts(email, password, firstName, lastName) VALUES (:email, :password, :firstName, :lastName)");
-
-            $query->bindParam(":email", $Email);
-            $query->bindParam(":password", $hashed_password);
-            $query->bindParam(":firstName", $FirstName);
-            $query->bindParam(":lastName", $LastName);
-
-            if ($query->execute()) {
-                $melding = "Gegevens zijn toegevoegd!";
-
-            }
-        }
-
-
-
-    }else {
-        $melding = "Vul alles in!";
-    }
-}
-
-=======
 global $checkLogin;
 $melding = '';
->>>>>>> Stashed changes
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -68,25 +21,22 @@ include_once ('defaults/header.php');
             <label for="email">Email:</label><br>
             <input type="email" id="input" class="border-bottom border-dark border-2" name="email"><br><br>
             <label for="email">Password:</label><br>
-            <input type="password" id="input" class="border-bottom border-dark border-2" name="password" id="password"><br><br>
+            <input type="password" id="input" class="border-bottom border-dark border-2" name="password"><br><br>
             <button type="submit" id="submit" name="register" class="d-flex border-bottom border-dark border-2" value="Login">Login</button><br>
             <?php echo $melding ?><br><br>
-<<<<<<< Updated upstream
-=======
             <?php
             $user = checkLogin();
 
+            echo $user;
             if ($user == "USER")
             {
                 header("admin/adminhome.php");
             }
             elseif ($user == "ADMIN")
             {
-                echo $user;
-                header('../public/admin.php');
+                header("location: ../admin.php");
             }
             ?>
->>>>>>> Stashed changes
         </form>
     </div>
 </div>

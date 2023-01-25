731e405fff9e5bb15dfcb1e5c4fc16f3c8b46bbc
RuiterM <302078305@student.rocmondriaan.nl>
2023-01-25 22:39:32 +0100

    Update login.php

--- Modules/login.php
+++ Modules/login.php
@@ -1,79 +1,79 @@
 <?php
-function checkLogin(): string
+
+function checkLogin():string
 {
     global $pdo;
-    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $email=filter_input(INPUT_POST,'email', FILTER_VALIDATE_EMAIL);
     $password=filter_input(INPUT_POST,'password');
 
-<<<<<<< Updated upstream
     if ($email!==false && !empty($password))
+    {
+        $sql = 'SELECT * FROM `accounts` WHERE `email` = :e AND `password` = :p';
+        $sql = $pdo->prepare($sql);
+        $sql->bindParam(':e',$email);
+        $sql->bindParam(':p', $password);
+        $sql->setFetchMode(PDO::FETCH_CLASS, 'User');
+        $sql->execute();
+        $user = $sql->fetch();
+
+        if ($user!==false)
         {
-            $sql = 'SELECT * FROM `accounts` WHERE `email` = :e AND `password` = :p';
-            $sql = $pdo->prepare($sql);
-            $sql->bindParam(':e',$email);
-            $sql->bindParam(':p',$password);
-            $sql->setFetchMode(PDO::FETCH_CLASS, 'User');
-            $sql->execute();
-            $user = $sth->fetch();
-
-            if ($user!==false)
+            $_SESSION['user']=$user;
+            if ($_SESSION['user']->role=="admin")
             {
-                $_SESSION['user']=$user;
-                if ($_SESSION['usr']->role=="admin")
-                {
-                    return 'ADMIN';
-                }
-                else
-                {
-                    return 'MEMBER';
-                }
+                return 'ADMIN';
             }
-            return 'FAILURE';
-=======
-    if ($email !== false && !empty($password)) {
-        $query = $pdo->prepare("SELECT * FROM `accounts` WHERE `email` = :u AND `password` = :p");
-        $query->bindParam(':u', $email);
-        $query->bindParam(':p', $password);
-        $query->setFetchMode(PDO::FETCH_CLASS, 'User');
-        $query->execute();
-        $user = $query->fetch();
-
-        if ($user) {
-            $_SESSION['user'] = $user;
-            if ($_SESSION['user']->role == "admin") {
-                return "ADMIN";
-            } else {
-                return "JAMMER";
+            else
+            {
+                return 'MEMBER';
             }
->>>>>>> Stashed changes
         }
-        return "FAILURE";
+        else
+        {
+            echo "ASDDDD";
+        }
+        return 'FAILURE';
     }
-    return "INCOMPLETE";
+    return 'INCOMPLETE';
 }
 
-function isAdmin(): bool
+function isAdmin():bool
 {
-    if (isset($_SESSION['user']) && !empty($_SESSION['user'])) {
-        $user = $_SESSION['user'];
-        if ($user->role == "admin") {
+    //controleer of er ingelogd is en de user de rol admin heeft
+    if(isset($_SESSION['user'])&&!empty($_SESSION['user']))
+    {
+        $user=$_SESSION['user'];
+        if ($user->role == "admin")
+        {
             return true;
-        } else {
+        }
+        else
+        {
             return false;
         }
     }
     return false;
 }
 
-function isMember(): bool
+function isMember():bool
 {
-    if (isset($_SESSION['user']) && !empty($_SESSION['user'])) {
-        $user = $_SESSION['user'];
-        if ($user->role === "member") {
+    //controleer of er ingelogd is en de user de rol admin heeft
+    if(isset($_SESSION['user'])&&!empty($_SESSION['user']))
+    {
+        $user=$_SESSION['user'];
+        if ($user->role === "member")
+        {
             return true;
-        } else {
+        }
+        else
+        {
             return false;
         }
     }
     return false;
+}
+
+function makeRegistration():string
+{
+
 }
